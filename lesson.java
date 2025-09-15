66863077b64d26b509281309510f810fb41200db
Nikolay Sokolov <nsokolovqa@outlook.com>
2025-09-15 19:08:50 +0300

    мучаюсь с гитом продолжение

--- lesson.java
+++ lesson.java
@@ -1,5 +1,7 @@
 public class lesson {
     public static void main(String[] args) {
         System.out.println("Привет,мир!");// комментарий к первой строке кода на джава
+        int index = 1;
+        String textEdit = "someText";
     }
 }
