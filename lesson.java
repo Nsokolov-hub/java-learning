908a289ec8a021f1e81eabfa0e3b329ddf46c97e
Nikolay Sokolov <nsokolovqa@outlook.com>
2025-09-15 18:43:24 +0300

    изучаю комментарии v2

--- lesson.java
+++ lesson.java
@@ -1,5 +1,5 @@
 public class lesson {
     public static void main(String[] args) {
-        System.out.println("Привет,мир!");
+        System.out.println("Привет,мир!");// комментарий к первой строке кода на джава
     }
 }
