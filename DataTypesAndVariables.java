43feae9a44a4b8dfad8107e6fc0e7b41b02be364
Nikolay Sokolov <nsokolovqa@outlook.com>
2025-09-16 11:53:25 +0300

    Изучаю типы данных продолжение

--- DataTypesAndVariables.java
+++ DataTypesAndVariables.java
@@ -4,5 +4,21 @@ public class DataTypesAndVariables {
         System.out.println(age);
         age = 33;
         System.out.println(age);
+        // числа целые и вещественные 
+        // int byte short long
+        int valueInt = 2; // - 2 147 483 648 .. 2 147 483 647
+        byte byteValue = 127; // -128...127
+        short valueShort = 32677; // -32768 ... 32677
+        long valueLong = 2; // тут просто милллиарды....
+        // вещественные типы
+        float valueFloat = 123.456f; // 32 бита
+        double valueDouble =  123.456; // 64 бита
+        // строки "Hello World"
+        // символы
+        char valueChar = '1';
+        // логический - true/false ключевое слово boolean 
+
+         
+
     }
 }
