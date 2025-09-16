0500b7dacf3e90f848ff7554d2499c1462bba2e5
Nikolay Sokolov <nsokolovqa@outlook.com>
2025-09-16 11:24:21 +0300

    Изучаю типы данных

---
+++ DataTypesAndVariables.java
@@ -0,0 +1,8 @@
+public class DataTypesAndVariables {
+    public static void main(String[] args) {
+        var age = 32;
+        System.out.println(age);
+        age = 33;
+        System.out.println(age);
+    }
+}
